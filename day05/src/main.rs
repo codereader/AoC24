3e7094dae26c7991540032861a96f5bce85cb4a1
codereader <greebo@angua.at>
2024-12-05 07:11:14 +0100

    Day05: Part 1

---
+++ day05/src/main.rs
@@ -0,0 +1,140 @@
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::io::Read;
+use std::fs::File;
+
+fn main() {
+    // Create an empty mutable string
+    let _test_content = "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47";
+    
+    // Read a file in the local file system
+    let mut data_file = File::open("src/input.txt").unwrap();
+    let mut file_content = String::new();
+    // Copy contents of file to a mutable string
+    data_file.read_to_string(&mut file_content).unwrap();
+    
+    let normalized_file = file_content.replace('\r', "");
+    let lines = normalized_file.split('\n');
+
+    let mut rules_before: HashMap<i32, HashSet<i32>> = HashMap::new();
+    let mut rules_after: HashMap<i32, HashSet<i32>> = HashMap::new();
+
+    let mut sum_part1 = 0;
+
+    let mut i = lines;
+    loop {
+        let line = i.next();
+
+        if line.is_none() || line.unwrap().trim().is_empty() {
+            break;
+        }
+
+        //println!("{}", line.unwrap());
+        let pieces: Vec<_> = line.unwrap().split('|').collect();
+        let key = pieces[0].parse::<i32>().expect("Parser error");
+        let value = pieces[1].parse::<i32>().expect("Parser error");
+
+        let before_set = rules_before.entry(key).or_insert_with(|| HashSet::new());
+        before_set.insert(value);
+
+        let after_set = rules_after.entry(value).or_insert_with(|| HashSet::new());
+        after_set.insert(key);
+    }
+
+    // for pair in rules.iter() {
+    //     println!("Key: {0}, Value {1:?}", pair.0, pair.1);
+    // }
+
+    println!("Evaluating Pages...");
+
+    loop {
+        let line = i.next();
+
+        if line == None || line.unwrap().is_empty() {
+            break;
+        }
+        
+        let pages:Vec<_> = line.unwrap().split(',').map(|x| x.parse::<i32>().expect("Parse error")).collect();
+        
+        println!("{:?}", pages);
+
+        let mut is_ok = true;
+
+        for i in 0..pages.len() {
+            for left in 0..i {
+                //print!("{0} must be after {1}: ", pages[i], pages[left]);
+
+                let rules_for_page = rules_after.get(&pages[i]);
+
+                if rules_for_page.is_none() {
+                    continue;
+                }
+
+                if rules_for_page.unwrap().contains(&pages[left]) {
+                    //println!("OK");
+                }
+                else {
+                    //println!("Rule Violation");
+                    is_ok = false;
+                }
+            }
+
+            let rules_for_page = rules_before.get(&pages[i]);
+
+            if rules_for_page.is_none() {
+                continue;
+            }
+
+            for right in i+1..pages.len() {
+                //print!("{0} must be before {1}: ", pages[i], pages[right]);
+
+                if rules_for_page.unwrap().contains(&pages[right]) {
+                    //println!("OK");
+                }
+                else {
+                    //println!("Rule Violation");
+                    is_ok = false;
+                }
+            }
+        }
+
+        if is_ok {
+            println!("Page is OK");
+            let middle_index = pages.len() / 2;
+            //println!("Middle Page: {0}", pages[middle_index]);
+            sum_part1 += pages[middle_index];
+        }
+        else {
+            println!("Page FAILED");
+        }
+    }
+
+    println!("[Part1]: Sum of middle page numbers = {0}", sum_part1);
+}
