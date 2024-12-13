cf8bf687f16525ff0d3564a52d80c961d0b21062
codereader <greebo@angua.at>
2024-12-13 09:22:59 +0100

    Day13: Part 2 complete

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,83 +1,7 @@
-use std::collections::{HashMap, HashSet};
 use std::io::Read;
 use std::fs::File;
-use std::hash::Hash;
-use std::ops::Add;
 use regex::Regex;
 use std::cmp::min;
-
-struct Vector2 {
-    x: i32,
-    y: i32,
-}
-
-impl Vector2 {
-    fn add(&self, other: &Self) -> Self {
-        Vector2 {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
-    }
-
-    fn add_by_value(&self, other: Self) -> Self {
-        Vector2 {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
-    }
-
-    fn from(tuple: &(i32, i32)) -> Vector2 {
-        return Vector2 { x: tuple.0, y: tuple.1 }; 
-    }
-
-    const fn North() -> Vector2 { return Vector2 { x: 0, y: -1 }; }
-    const fn South() -> Vector2 { return Vector2 { x: 0, y: 1 }; }
-    const fn East() -> Vector2 { return Vector2 { x: 1, y: 0 }; }
-    const fn West() -> Vector2 { return Vector2 { x: -1, y: 0 }; }
-}
-
-impl<'a, 'b> Add<&'b Vector2> for &'a Vector2 {
-    type Output = Vector2;
-
-    fn add(self, other: &'b Vector2) -> Vector2 {
-        Vector2 {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
-    }
-}
-
-impl<'a, 'b> Add<Vector2> for &'a Vector2 {
-    type Output = Vector2;
-
-    fn add(self, other: Vector2) -> Vector2 {
-        Vector2 {
-            x: self.x + other.x,
-            y: self.y + other.y,
-        }
-    }
-}
-
-impl Hash for Vector2 {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.x.hash(state);
-        self.y.hash(state);
-    }
-}
-
-impl Eq for Vector2 {}
-
-impl PartialEq for Vector2 {
-    fn eq(&self, other: &Self) -> bool {
-        self.x == other.x && self.y == other.y
-    }
-}
-
-impl Clone for Vector2 {
-    fn clone(&self) -> Vector2 {
-        Vector2 { x: self.x, y: self.y }
-    }
-}
 
 fn main() {
     // Create an empty mutable string
@@ -116,7 +40,7 @@ Prize: X=18641, Y=10279";
     let price_regex = Regex::new(r"X=(\d+), Y=(\d+)").unwrap();
 
     let num_machines = lines.len() / 3;
-    println!("Machines: {num_machines}");
+    //println!("Machines: {num_machines}");
 
     for index in 0..num_machines {
         let button_a = button_regex.captures_iter(lines[index*3]).map(|m| m).next().unwrap();
@@ -128,7 +52,7 @@ Prize: X=18641, Y=10279";
         let b = (&button_b[1].parse::<i64>().unwrap(), &button_b[2].parse::<i64>().unwrap());
         let price = (&price_capture[1].parse::<i64>().unwrap(), &price_capture[2].parse::<i64>().unwrap());
 
-        println!("A = {:?}, B = {:?}, P = {:?}", a, b, price);
+        //println!("A = {:?}, B = {:?}, P = {:?}", a, b, price);
 
         let max_a = min(price.0 / a.0, 100);
         let max_b = min(price.0 / b.0, 100);
@@ -137,30 +61,49 @@ Prize: X=18641, Y=10279";
         for ax in 0..max_a+1 {
             for bx in 0..max_b+1  {
                 if ax * a.0 + bx * b.0 == *price.0 && ax * a.1 + bx * b.1 == *price.1 {
-                    println!("Solution: {0}xA {1}xB, Cost = {2}", ax, bx, ax * 3 + bx * 1);
+                    //println!("Solution: {0}xA {1}xB, Cost = {2}", ax, bx, ax * 3 + bx * 1);
                     solutions.push(ax * 3 + bx * 1);
                 }
             }
         }
 
         if solutions.len() == 0 {
-            println!("No solution");
+            // no solution
+        }
+        else if solutions.len() > 1 {
+            panic!("More than one solution");
         }
         else {
-            if solutions.len() > 1 {
-                println!("More than one solution");
-            }
             solutions.sort();
             let min_cost = solutions.iter().next().unwrap();
             sum_part1 += min_cost;
-            println!("Minimum cost: {0}", min_cost);
+            //println!("Minimum cost: {0}", min_cost);
         }
-    }
 
+        // Part 2: solve the linear equations
+        let px = *price.0 as f64 + 10000000000000.0;
+        let py = *price.1 as f64 + 10000000000000.0;
+        let ax = *a.0 as f64;
+        let ay = *a.1 as f64;
+        let bx = *b.0 as f64;
+        let by = *b.1 as f64;
+
+        let count_b = (px * ay - py * ax) / (bx * ay - by * ax);
+        let count_a = (px - bx * count_b) / ax;
+
+        if count_b.fract() > 0.0 || count_a.fract() > 0.0 {
+            //println!("No solution");
+            continue;
+        }
+
+        //println!("A times {0}, B times {1}", count_a, count_b);
+
+        sum_part2 += (count_a as i64) * 3 + (count_b as i64) * 1;
+    }
 
     let elapsed = now.elapsed();
 
-    println!("[Part1]: Minimum cost = {0}", sum_part1); // ???
-    println!("[Part2]: ... = {0}", sum_part2); // ???
+    println!("[Part1]: Minimum cost = {0}", sum_part1); // 26810
+    println!("[Part2]: Minimum cost = {0}", sum_part2); // 108713182988244
     println!("Elapsed Time: {:.2?}", elapsed);
 }
